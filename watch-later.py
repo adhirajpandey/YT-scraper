2c8335c861babf0f1970780369244b90fb5503bf
Adhiraj <pandey.adhiraj02@gmail.com>
2023-01-02 20:10:40 +0530

    Added custom csv or txt preference

--- watch-later.py
+++ watch-later.py
@@ -1,8 +1,8 @@
 #USER CONFIG
 
-WATCH_LATER_HTML = '[path/filename].html' # Path to watch later html file
-txt = 0 # 0 = no, 1 = yes
-csv = 1 # 0 = no, 1 = yes
+WATCH_LATER_HTML = 'wl.html' # Path to watch later html file
+TXT_FILE = 1 # 0 = no, 1 = yes
+CSV_FILE = 0 # 0 = no, 1 = yes
 
 #IMPORT LIBRARIES
 
@@ -23,12 +23,16 @@ def parse_html(htmlfile):
         x, y = (link['href'], link['title'])
         data[x] = y
 
+    print("HTML Page Parsed Successfully")
+
     return data
 
 def write_to_txt(data):
     with open('watchlater.txt', 'w', encoding='utf-8') as f:
         for key, value in data.items():
             f.write(f"{key} : {value}\n")
+    
+    print("TXT File Created Successfully")
 
 def write_to_csv(data):
     with open('watchlater.csv', 'w', encoding='utf-8', newline='') as f:
@@ -36,16 +40,19 @@ def write_to_csv(data):
         writer.writerow(['Title','Link'])
         for key, value in data.items():
             writer.writerow([value, key])
+    
+    print("CSV File Created Successfully")
 
 if __name__ == '__main__':
     
     data = parse_html(WATCH_LATER_HTML)
 
-    if txt == 1:
+    if TXT_FILE == 1:
         write_to_txt(data)
     
-    if csv == 1:
+    elif CSV_FILE == 1:
         write_to_csv(data)
 
-    if txt == 0 and csv == 0:
+    elif TXT_FILE == 0 and CSV_FILE == 0:
         print("Enter valid option")
+    
